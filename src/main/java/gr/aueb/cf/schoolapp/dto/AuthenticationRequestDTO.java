91b8b34e7fa719b29ea96b2248198800192c5a8c
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-10-31 21:00:33 +0200

    authentication and jwtservice

---
+++ src/main/java/gr/aueb/cf/schoolapp/dto/AuthenticationRequestDTO.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.schoolapp.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequestDTO {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+}
---
+++ src/main/java/gr/aueb/cf/schoolapp/dto/AuthenticationResponseDTO.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.schoolapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponseDTO {
+
+    private String firstname;
+    private String lastname;
+    private String token;
+}
